9283e976a5b2db7a27167d51a4374920ea589770
Miroslav Michalicka <miro.michalicka@gmail.com>
2016-12-07 21:43:18 +0100

    Add drush alias

---
+++ drush/local.aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+
+$aliases["dev"] = array (
+  'root' => '/var/www/drupal/public_html/web',
+  'uri' => 'http://beliana.docker.amazee.io',
+  'path-aliases' => 
+  array (
+    '%drush' => '/var/www/drupal/public_html/vendor/drush/drush',
+    '%site' => 'sites/dev/',
+  ),
+);
