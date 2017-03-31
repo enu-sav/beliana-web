102600fe9d8dd7668e351191234a8b00d501c7fe
Miroslav Michalicka <miro.michalicka@gmail.com>
2017-03-31 08:08:30 +0200

    Configuration modification from stage and injecting configuration through settings.

--- web/sites/stage/settings.php
+++ web/sites/stage/settings.php
@@ -18,6 +18,20 @@ $settings['hash_salt'] = 'J2RATdd4h4GdghkCZH99vaNPXR2-6_q0m6fC1qlrYm6SzkpAp48cDb
 $settings['install_profile'] = 'standard';
 $config_directories['sync'] = '../config/sync';
 
+// SOLR settings.
+$config['search_api.server.solr']['backend_config']['connector_config']['host'] = 'localhost';
+$config['search_api.server.solr']['backend_config']['connector_config']['path'] = '/solr/';
+$config['search_api.server.solr']['backend_config']['connector_config']['core'] = 'drupal';
+$config['search_api.server.solr']['backend_config']['connector_config']['port'] = 8983;
+$config['search_api.server.solr']['backend_config']['connector_config']['http_user'] = '';
+$config['search_api.server.solr']['backend_config']['connector_config']['http']['http_user'] = '';
+$config['search_api.server.solr']['backend_config']['connector_config']['http_pass'] = '';
+$config['search_api.server.solr']['backend_config']['connector_config']['http']['http_pass'] = '';
+$config['search_api.server.solr']['name'] = 'Solr - Environment: stage';
+
+// Locale settings.
+$config['locale.settings']['translation']['path'] = 'sites/stage/files/translations';
+
 // Last: this servers specific settings files.
 if (file_exists(__DIR__ . '/settings.local.php')) {
   include __DIR__ . '/settings.local.php';
