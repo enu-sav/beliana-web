f05196df02812acfb018557ced1b1ddd2ec1423e
Miroslav Michalicka <miro.michalicka@gmail.com>
2017-04-22 16:52:45 +0200

    Move search to separated view.

--- web/themes/custom/cool/js/word-search.js
+++ web/themes/custom/cool/js/word-search.js
@@ -1,16 +1,18 @@
-jQuery(document).ready(function($) {
-    // Set word search facet api second level filter position.
-    $('#block-heslo ul li ul').css("top", $('#block-heslo').height() + 8);
-    $('.word-facet-wrap').height($('#block-heslo').height() + $('#block-heslo ul li ul').height());
+(function ($) {
 
-    $(window).resize(function() {
+  Drupal.behaviors.wordFacetsBlock = {
+    attach: function () {    // Set word search facet api second level filter position.
+      $('.word-facet-wrap').height($('#block-heslo').height() + $('#block-heslo ul li ul').height());
+
+      $(window).resize(function () {
         $('.word-facet-wrap').height(0);
-        $('#block-heslo ul li ul').css("top", $('#block-heslo').height() + 7);
         $('.word-facet-wrap').height($('#block-heslo').height() + $('#block-heslo ul li ul').height());
-    });
+      });
 
-    // Set Heslá string in solr_search_word view header.
-    if ($('#block-heslo .facet-item .is-active .facet-item__value')) {
-        $('.view-word-search-page header').prepend( '<h2>' + 'Heslá ' + $('#block-heslo .facet-item .is-active .facet-item__value').text() + '</h2>');
+      // Set active facet string in solr_search_word view header.
+      if ($('#block-heslo .facet-item .is-active .facet-item__value')) {
+        $('.view-word-search-page header h2').append(' ' + $('#block-heslo .facet-item .is-active .facet-item__value').text());
+      }
     }
-});
+  }
+})(jQuery);
