28e5b318e585b0ed7b0b815343dc72788fc5264a
Miroslav Michalicka <miro.michalicka@gmail.com>
2017-04-27 13:01:21 +0200

    Fix active facet being add multiple times and add JS results counter.

--- web/themes/custom/cool/js/word-search.js
+++ web/themes/custom/cool/js/word-search.js
@@ -1,18 +1,29 @@
 (function ($) {
 
-  Drupal.behaviors.wordFacetsBlock = {
-    attach: function () {    // Set word search facet api second level filter position.
+  Drupal.behaviors.wordFacetsBlockTheme = {
+    attach: function () {
+      // Set word search facet api second level filter position.
       $('.word-facet-wrap').height($('#block-heslo').height() + $('#block-heslo ul li ul').height());
 
       $(window).resize(function () {
         $('.word-facet-wrap').height(0);
         $('.word-facet-wrap').height($('#block-heslo').height() + $('#block-heslo ul li ul').height());
       });
+    }
+  };
 
+  Drupal.behaviors.setCurrentWordFacet = {
+    attach: function () {
       // Set active facet string in solr_search_word view header.
       if ($('#block-heslo .facet-item .is-active .facet-item__value')) {
-        $('.view-word-search-page header h2').append(' ' + $('#block-heslo .facet-item .is-active .facet-item__value').text());
+        $('.view-word-search-page header h2').once().append(' ' + $('#block-heslo .facet-item .is-active .facet-item__value').text());
       }
+    }
+  };
+
+  Drupal.behaviors.wordCounter = {
+    attach: function () {
+      $('#count-words').text($('.view-word-search-page .views-row').size());
     }
   }
 })(jQuery);
