17dc42ef88b0e9f10ebd9c7fa9fffb41e7b0d612
Peter <peter.hrabovcin@gmail.com>
2017-05-01 18:06:57 +0200

    Make tables responsive

--- web/themes/custom/cool/js/word.js
+++ web/themes/custom/cool/js/word.js
@@ -12,4 +12,12 @@
         }
     };
 
+    Drupal.behaviors.tableFade = {
+        attach: function () {
+            if ($('.table-500').width() > $('.table-container-outer').width()) {
+                $('.table-container-outer div:first-child').addClass('table-container-fade');
+            }
+        }
+    };
+
 })(jQuery);
