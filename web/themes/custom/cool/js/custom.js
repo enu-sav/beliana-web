4fc89f11b12288089812892ddc896a2e7d7bbd51
Peter <peter.hrabovcin@gmail.com>
2017-05-01 22:00:47 +0200

    Make table responsive - fix 2

--- web/themes/custom/cool/js/custom.js
+++ web/themes/custom/cool/js/custom.js
@@ -20,9 +20,11 @@
 
     Drupal.behaviors.tableFade = {
         attach: function () {
-            if ($('.table-500').width() > $('.table-container-outer').width()) {
-                $('.table-container-outer div:first-child').addClass('table-container-fade');
-            }
+            $(window).on('load resize', function () {
+                if ($('.table-500').width() > $('.table-container-outer').width()) {
+                    $('.table-container-outer div:first-child').addClass('table-container-fade');
+                }
+            });
         }
     };
 
