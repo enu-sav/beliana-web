9112153812a1cfa0682857c96cf0f9b6c3bdc79e
Matej Lehotský <lehotsky@synergiq.sk>
2017-12-19 13:01:42 +0100

    Update template style and configuration

--- web/themes/custom/beliana/js/override.js
+++ web/themes/custom/beliana/js/override.js
@@ -8,7 +8,12 @@
 
   Drupal.behaviors.override = {
     attach: function (context, settings) {
+      $('.heslo-tools .print').on('click', function (e) {
+        e.preventDefault();
 
+        $('article .citacia h3 a').click();
+        window.print();
+      });
     }
   };
 
