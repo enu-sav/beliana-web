79a2e5f8331db6531272456ba58b79e07a276e93
Matej Lehotský <lehotsky@synergiq.sk>
2018-01-05 11:55:28 +0100

    Added images size calculation in js

--- web/themes/custom/beliana/js/override.js
+++ web/themes/custom/beliana/js/override.js
@@ -14,6 +14,24 @@
         $('article .citacia h3 a').click();
         window.print();
       });
+
+      //set "Ilustracia" image media size in "Heslo" node
+      $('.heslo-ilustracia .media-image.view-mode-in-word img').each(function (key, value) {
+        Drupal.behaviors.override.setMediaImageSize($(value), 420);
+      });
+
+      $('article.media-image.view-mode-full img').each(function (key, value) {
+        Drupal.behaviors.override.setMediaImageSize($(value), 660);
+      });
+    },
+    setMediaImageSize: function (image, maxHeight) {
+      var ratio = image.height() / image.width();
+
+      image.css({width: '100%'});
+
+      if (ratio > 1 && image.parent().width() * ratio > maxHeight) {
+        image.css({height: maxHeight + 'px', width: 'auto'});
+      }
     }
   };
 
