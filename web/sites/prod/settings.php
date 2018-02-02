704abc5b31180bfa1b55d410866c9caa379deccb
Matej Lehotský <lehotsky@synergiq.sk>
2018-02-02 12:36:51 +0100

    Update settings files to disable caching

--- web/sites/prod/settings.php
+++ web/sites/prod/settings.php
@@ -1,10 +1,9 @@
 <?php
 
 /**
-* @file
-* Settings for staging environment. Same will be used for production.
-*/
-
+ * @file
+ * Settings for staging environment. Same will be used for production.
+ */
 $settings['hash_salt'] = 'J2RATdd4h4GdghkCZH99vaNPXR2-6_q0m6fC1qlrYm6SzkpAp48cDboGnEC6iQy940JhA9rzcA';
 $settings['install_profile'] = 'standard';
 $config_directories['sync'] = '../config/sync';
@@ -24,6 +23,10 @@ $config['search_api.server.solr']['name'] = 'Solr - Environment: prod';
 $config['config_split.config_split.dev']['status'] = FALSE;
 $config['config_split.config_split.stage']['status'] = FALSE;
 $config['config_split.config_split.prod']['status'] = TRUE;
+
+// Disable render caches, necessary for twig files to be reloaded on every page view
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 
 // Last: this servers specific settings files.
 if (file_exists(__DIR__ . '/settings.local.php')) {
--- web/sites/stage/settings.php
+++ web/sites/stage/settings.php
@@ -1,10 +1,9 @@
 <?php
 
 /**
-* @file
-* Settings for staging environment. Same will be used for production.
-*/
-
+ * @file
+ * Settings for staging environment. Same will be used for production.
+ */
 // Don't show any error messages on the site (will still be shown in watchdog).
 $config['system.logging']['error_level'] = 'hide';
 
@@ -34,6 +33,10 @@ $config['search_api.server.solr']['name'] = 'Solr - Environment: stage';
 $config['config_split.config_split.dev']['status'] = FALSE;
 $config['config_split.config_split.stage']['status'] = TRUE;
 $config['config_split.config_split.prod']['status'] = FALSE;
+
+// Disable render caches, necessary for twig files to be reloaded on every page view
+$settings['cache']['bins']['render'] = 'cache.backend.null';
+$settings['cache']['bins']['dynamic_page_cache'] = 'cache.backend.null';
 
 // Last: this servers specific settings files.
 if (file_exists(__DIR__ . '/settings.local.php')) {
