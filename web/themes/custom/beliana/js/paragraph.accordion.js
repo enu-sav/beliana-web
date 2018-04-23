1f6c5110665e6a80b7a3155ee04b91f1df16ab5d
Matej Lehotský <lehotsky@synergiq.sk>
2018-04-23 10:16:56 +0200

    Added Paragraphs and create custom types

---
+++ web/themes/custom/beliana/js/paragraph.accordion.js
@@ -0,0 +1,34 @@
+/**
+ * @file
+ */
+
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.paragraphAccordion = {
+    attach: function (context, settings) {
+      var $context = $(context);
+
+      $context.find('.paragraph--type--accordion').once('paragraph-accordion-process').each(function () {
+        var sidebar = $('.sidebar-menu ul', this);
+        var content = $('.content .field--name-field-section');
+
+        sidebar.on('click', 'li a', function (e) {
+          e.preventDefault();
+          
+          console.log($(this).attr('href'));
+
+          sidebar.find('li a').removeClass('active');
+          $(this).addClass('active');
+
+          $('html, body').animate({
+            scrollTop: content.find($(this).attr('href')).offset().top - 100
+          }, 500);
+        });
+      });
+
+    }
+  };
+
+})(jQuery, Drupal);
