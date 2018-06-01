180579ca3aadcf0e20df00ad92c87ced4a00b0aa
Matej Lehotský <lehotsky@synergiq.sk>
2018-06-01 12:15:09 +0200

    Update sticky sidebar menu behavior in Paragraph accordion

--- web/themes/custom/beliana/js/paragraph.accordion.js
+++ web/themes/custom/beliana/js/paragraph.accordion.js
@@ -13,6 +13,21 @@
       $context.find('.paragraph--type--accordion').once('paragraph-accordion-process').each(function () {
         var sidebar = $('.sidebar-menu ul', this);
         var content = $('.content .field--name-field-section');
+        var window_height = $(window).height();
+
+        if (sidebar.height() > window_height - $('header.header').height() - 70) {
+          sidebar.css({position: 'relative'});
+        } else {
+          var footer_position_top = $('footer').position().top;
+
+          $(window).scroll(function () {
+            if ($(window).scrollTop() > footer_position_top + sidebar.height() - $('footer').height() - window_height) {
+              sidebar.css({position: 'relative', top: (footer_position_top - $('header.header').height() - $('footer').height() - sidebar.height() - 50) + 'px'});
+            } else {
+              sidebar.css({position: 'fixed', top: ''});
+            }
+          });
+        }
 
         sidebar.on('click', 'li a', function (e) {
           e.preventDefault();
