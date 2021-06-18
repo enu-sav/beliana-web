0972356cd0b2df5d4fcc27dcab54108604339e5d
Matej Lehotský <lehotsky@improve.sk>
2021-06-18 07:39:49 +0200

    BEL-3 Update search api from dev to stable

--- web/sites/dev/settings.php
+++ web/sites/dev/settings.php
@@ -1,18 +1,5 @@
 <?php
 
-//$databases['default'] = array(
-//  'default' => array(
-//    'database' => 'drupal',
-//    'username' => 'drupal',
-//    'password' => 'drupal',
-//    'prefix' => '',
-//    'host' => 'localhost',
-//    'port' => '3306',
-//    'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-//    'driver' => 'mysql',
-//  )
-//);
-
 // Show all error messages on the site
 $config['system.logging']['error_level'] = 'all';
 // Disable Google Analytics from sending dev GA data.
