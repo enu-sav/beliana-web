1a152f99fc9ee8f509ec71364f8d243e6d82f8db
Matej Lehotský <lehotsky@improve.sk>
2022-02-21 15:11:08 +0100

    BEL-57 Update sites folder and config

--- drush/local.aliases.drushrc.php
+++ drush/local.aliases.drushrc.php
@@ -3,10 +3,10 @@
 $aliases["dev"] = array (
   'root' => '/var/www/drupal/public_html/web',
   'uri' => 'http://beliana.docker.amazee.io',
-  'path-aliases' => 
+  'path-aliases' =>
   array (
     '%drush' => '/var/www/drupal/public_html/vendor/drush/drush',
-    '%site' => 'sites/dev/',
+    '%site' => 'sites/default/',
   ),
 );
 
