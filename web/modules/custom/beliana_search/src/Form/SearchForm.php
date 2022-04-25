d93484b0de6b6f237e8a4272297b2cb75efe8ae5
Peter Sykora <sykora@improve.sk>
2022-04-25 10:11:19 +0200

    BEL-33 - fixed by audit - citation.

--- web/modules/custom/beliana_search/src/Form/SearchForm.php
+++ web/modules/custom/beliana_search/src/Form/SearchForm.php
@@ -44,6 +44,7 @@ class SearchForm extends FormBase {
         '#attributes' => [
           'class' => ['search-input'],
           'placeholder' => 'Vyhľadať (pozri Možnosti vyhľadávania vpravo)',
+          'aria-label' => 'Vyhľadať (Pre ďalšie možnosti vyhľadávania, pokračujte na prvok Nápoveda vyhľadávania)',
         ],
       ]
     ];
--- web/themes/custom/beliana/js/black-white.js
+++ web/themes/custom/beliana/js/black-white.js
@@ -8,14 +8,14 @@
 
   /** Black and white version **/
   const html = document.getElementsByTagName('html')[0];
-  var black_white = getCookie('black-white') || 'normal';
+  var black_white = localStorage.getItem('black-white') || 'normal';
 
   if (black_white === 'black-white') {
     html.classList.add('black-white');
-    setCookie('black-white', 'black-white', 30)
+    localStorage.setItem('black-white', 'black-white')
   } else {
     html.classList.remove('black-white');
-    setCookie('black-white', 'normal', 30)
+    localStorage.setItem('black-white', 'normal')
   }
 
   function getCookie(cName) {
