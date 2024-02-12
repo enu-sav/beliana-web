dd8cc1b9e4be5928ebb168c71627d9041d3f7318
matej.choma <matej.choma5@gmail.com>
2024-02-12 14:38:16 +0100

    BEL-128 added texts for homepage

---
+++ tests/cypress/support/variables/footer/textsForHomePage.ts
@@ -0,0 +1,20 @@
+export const texts = {
+  aboutBeliane: 'O Beliane',
+  searchOptions: 'Možnosti vyhľadávania',
+  bodyLinkText: 'Slovenská všeobecná encyklopédia – viac o encyklopédii',
+  anniversaries: 'Výročia',
+  linkForAnniversaries: 'Zobraziť všetky výročia',
+  msgForEmptyAnniversariesList: 'K dnešnému dátumu sa neviažu žiadne heslá.',
+  latestWords: 'Nové heslá',
+  linkForLatestWords: 'Zobraziť viac nových hesiel',
+  headerTopTexts: [
+    'Slovenská akadémia vied',
+    'Kontakt'],
+  footerLinksText: [
+    'Autorský kolektív',
+    'Podmienky použitia',
+    'Encyclopaedia Beliana',
+    'Vyhlásenie o prístupnosti',
+    'Vyhľadávanie podľa kategórií'
+  ]
+}
